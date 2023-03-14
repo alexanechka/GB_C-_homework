fd3a006d23b652b8c0ca6c1a37743a8b94ded2e5
alexanechka <alexanechka@gmail.com>
2023-03-14 21:34:57 +0300

    hw04

---
+++ hw04/hw04_02/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadUserInput(string userString)
+{
+    Console.WriteLine(userString);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int SumDigit(int num)
+{
+    int num_to_parce = num;
+    int sum = 0;
+    while (num_to_parce > 0)
+    {
+        sum = sum + num_to_parce % 10;
+        num_to_parce = num_to_parce / 10;
+    }
+    return sum;
+}
+
+int number = ReadUserInput("Введите число ");
+int summ = SumDigit(number);
+Console.WriteLine(summ);
---
+++ hw04/hw04_03/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+int[] GetArray()
+{
+    int[] arr = new int[8];
+    for (int i = 0; i< 8; i++)
+    {
+        arr[i] = Random.Shared.Next(0, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i > 0)
+        {
+            Console.Write(", ");  
+        }
+        Console.Write(array[i]);  
+    }
+    Console.Write("]");
+}
+
+int[] randArray = GetArray();
+PrintArray(randArray);
