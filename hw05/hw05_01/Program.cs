ae41c1d7e70a69871954e3ea74cb345e1852d524
alexanechka <alexanechka@gmail.com>
2023-03-22 17:51:43 +0300

    tails

---
+++ hw05/hw05_01/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+ */
+
+int[] GetArray(int lenthArray)
+{
+    int[] arr = new int[lenthArray];
+    for (int i = 0; i < lenthArray; i++)
+    {
+        arr[i] = Random.Shared.Next(100, 1000);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i > 0)
+        {
+            Console.Write(", ");
+        }
+        Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int EvenCount(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            result = result + array[i];
+        }
+    }
+    return result;
+}
+
+int[] randArray = GetArray(4);
+PrintArray(randArray);
+Console.WriteLine(EvenCount(randArray));
---
+++ hw05/hw05_03/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+double[] GetArray(int lenthArray)
+{
+    double[] arr = new double[lenthArray];
+    for (int i = 0; i < lenthArray; i++)
+    {
+        arr[i] = Random.Shared.NextDouble();
+    }
+    return arr;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i > 0)
+        {
+            Console.Write(", ");
+        }
+        Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+double DeltaMaxMin(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max - min;
+}
+
+double[] randArray = GetArray(4);
+PrintArray(randArray);
+Console.WriteLine(DeltaMaxMin(randArray));
