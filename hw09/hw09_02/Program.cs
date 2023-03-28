fdd45a18bf226f5ed1ec8424d49e28b57251f2e2
alexanechka <alexanechka@gmail.com>
2023-03-29 00:00:15 +0300

    hw09

---
+++ hw09/hw09_02/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+
+int SumOfNumbersBetweenMAndN(int m, int n)
+{
+    if (m == n) return m;
+    if (n < m) return SumOfNumbersBetweenMAndN(n, m);
+    return n + SumOfNumbersBetweenMAndN(m, n - 1);
+}
+
+Console.WriteLine(SumOfNumbersBetweenMAndN(1, 15));
+Console.WriteLine(SumOfNumbersBetweenMAndN(15, 1));
+Console.WriteLine(SumOfNumbersBetweenMAndN(4, 8));
+Console.WriteLine(SumOfNumbersBetweenMAndN(8, 8));
